94924ef583fd7b7c810a3e11a5ebe6f0943d5fa5
Nazar <nazarzajcev3@gmail.com>
2026-05-09 23:53:28 +0300

    feat(api): add BookElement model

---
+++ LiberInk_API/LiberInkAPI/Models/BookElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LiberInkAPI.Models
+{
+    public class BookElement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Subtitle { get; set; } // ? означає, що може бути NULL
+        public int Order { get; set; }
+        public string Type { get; set; } // 'Part' або 'Chapter'
+        public int BookId { get; set; }
+        public int? ParentId { get; set; }
+        public string? Content { get; set; }
+
+        // Ця властивість не зберігається в БД (через NotMapped), 
+        // але потрібна для того, щоб React міг відмалювати дерево (Part -> Chapters)
+        [NotMapped]
+        public List<BookElement> Children { get; set; } = new List<BookElement>();
+    }
+}
