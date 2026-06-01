bb7ceaa743e205e34b12c24a066aa38c615aacb9
Nazar <nazarzajcev3@gmail.com>
2026-06-01 21:19:16 +0300

    Optimize vite build for Vercel

--- LiberInk_Web/vite.config.ts
+++ LiberInk_Web/vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Вмикаємо новий Tailwind
+    tailwindcss(),
   ],
+  build: {
+    // Збільшуємо ліміт до 1000 кБ, щоб не падати від попереджень про розмір файлів
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        // Автоматичне розділення великих бібліотек на менші частини
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
