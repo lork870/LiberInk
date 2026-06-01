f549e6c75b7a02dbb2d8c1a4ef1095b898786b2d
Nazar <nazarzajcev3@gmail.com>
2026-06-02 01:09:51 +0300

    Update API base URL to Render production

--- LiberInk_Mobile/app/src/main/java/com/zaitsev/liberink/data/RetrofitClient.kt
+++ LiberInk_Mobile/app/src/main/java/com/zaitsev/liberink/data/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://172.16.14.174:5241/"
+    private const val BASE_URL = "https://liber-ink-api.onrender.com"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
